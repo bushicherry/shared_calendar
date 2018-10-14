ce4769c9000e4d2c67f5cff929c3f2bbecf78d6f
bushicherry <31837159+bushicherry@users.noreply.github.com>
2018-10-14 00:35:24 -0400

    new

--- src/distSys/Driver.java
+++ src/distSys/Driver.java
@@ -1,5 +1,6 @@
 package distSys;
 
+import com.sun.tools.corba.se.idl.StringGen;
 import javafx.util.Pair;
 
 import java.io.File;
@@ -40,17 +41,20 @@ public class Driver {
             System.out.println(entry.getKey() + " " + entry.getValue() + "\n");
         }*/
 
-        int myPort = hostsPorts.get(args[0]).getKey();
-        int myIndex = hostsPorts.get(args[0]).getValue();
+        final String myName = args[0];
+        final int myPort = hostsPorts.get(myName).getKey();
+        final int myIndex = hostsPorts.get(myName).getValue();
+        final int numOfHosts = numHosts;
         // set up log and dictionary
         final LogAndDic logAndDic = new LogAndDic(numHosts,myIndex);
+
 
         // set up udp socket on a new thread to listen for msgs from other sites
         Runnable udpListener = new Runnable() {
 
             @Override
             public void run() {
-                System.out.println(args[0] + ": start listening for msgs.");
+                System.out.println(myName + ": start listening for msgs.");
                 try {
                     DatagramSocket socket = new DatagramSocket(myPort);
 
@@ -60,8 +64,74 @@ public class Driver {
                             public synchronized void run() {
                                 byte[] buffer = new byte[65507];
                                 DatagramPacket packet = new DatagramPacket(buffer,0,buffer.length);
-                                socket.receive(packet);
-                                logAndDic.receive(buffer);
+                                try {
+                                    socket.receive(packet);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                    return;
+                                }
+
+                                // byte to sendPac
+                                boolean isCreate = false;
+                                byte[] tmp = Arrays.copyOfRange(buffer,0,64);
+                                String msg_recv = new String(tmp);
+                                int index_recv = buffer[64];
+                                int[][] Ti_recv = new int[numOfHosts][numOfHosts];
+                                for (int i=0; i<numOfHosts; i++) {
+                                    for (int j=0; j<numOfHosts; j++) {
+                                        Ti_recv[i][j] = buffer[65+i*numOfHosts+j];
+                                    }
+                                }
+                                Vector<LogAndDic.eRecord> NP_recv = new Vector<>();
+
+                                int offset = 65+numOfHosts*numOfHosts;
+                                byte comma = ',';
+                                while (buffer[offset]==comma) {
+                                    int ts_recv = buffer[offset+1];
+                                    ts_recv = ts_recv*128+buffer[offset+2];
+
+                                    int Pi_recv = buffer[offset+3];
+
+                                    tmp = Arrays.copyOfRange(buffer,offset+4,offset+24);
+                                    String name_recv = new String(tmp);
+
+                                    meetingInfo op_recv;
+
+                                    if (buffer[offset+24]!=comma) {
+                                        isCreate = true;
+                                        int month_recv = buffer[offset+24];
+                                        int day_recv = buffer[offset+25];
+                                        int year_recv = buffer[offset+26];
+                                        year_recv = year_recv*128+buffer[offset+27];
+                                        int shour_recv = buffer[offset+28];
+                                        int smin_recv = buffer[offset+29];
+                                        int ehour_recv = buffer[offset+30];
+                                        int emin_recv = buffer[offset+31];
+
+                                        offset = offset+32;
+                                        Vector<String> users_recv = new Vector<>();
+                                        while (buffer[offset]==comma) {
+                                            tmp = Arrays.copyOfRange(buffer,offset+1,offset+26);
+                                            users_recv.add(new String(tmp));
+                                            offset += 26;
+                                        }
+
+                                        op_recv = new meetingInfo(name_recv,
+                                                new GregorianCalendar(year_recv, month_recv, day_recv),
+                                                new GregorianCalendar(year_recv, month_recv, day_recv, shour_recv, smin_recv),
+                                                new GregorianCalendar(year_recv, month_recv, day_recv, ehour_recv, emin_recv),
+                                                users_recv);
+                                        offset++;
+                                    } else {
+                                        op_recv = new meetingInfo(name_recv);
+                                        offset += 25;
+                                    }
+
+                                    NP_recv.add(new LogAndDic.eRecord(op_recv, ts_recv, Pi_recv));
+                                }
+                                LogAndDic.sendPac pac = new LogAndDic.sendPac(msg_recv,Ti_recv,NP_recv,index_recv);
+
+                                logAndDic.dealWithReceive(pac);
                             }
                         };
 
@@ -98,7 +168,7 @@ public class Driver {
                 Vector<String> participants = new Vector<>();
                 boolean selfIncluded = false;
                 while (commandS.hasNext()) {
-                    if (commandS.next().equals(args[0])) selfIncluded = true;
+                    if (commandS.next().equals(myName)) selfIncluded = true;
                     participants.add(commandS.next());
                 }
                 if (!selfIncluded) {
@@ -110,16 +180,8 @@ public class Driver {
                 GregorianCalendar gStartTime = new GregorianCalendar(date.getYear(),date.getMonth(),date.getDay(),startTime.getHour(),startTime.getMinute());
                 GregorianCalendar gEndTime = new GregorianCalendar(date.getYear(),date.getMonth(),date.getDay(),endTime.getHour(),endTime.getMinute());
                 meetingInfo m = new meetingInfo(name,gDate,gStartTime,gEndTime,participants);
-                //boolean conflict = false;
-                for (String p : participants) {
 
-                }
-                if (conflict) {
-                    System.out.println("Unable to schedule meeting " + name +".");
-                    continue;
-                } else {
-                    bluhbluh;
-                }
+                Algorithm.Insert(logAndDic,m);
 
 
             }
@@ -130,7 +192,7 @@ public class Driver {
                 logAndDic.View_dic();
             }
             else if (command.equals("myview")) {
-                logAndDic.myView(args[0]);
+                logAndDic.myView(myName);
             }
             else if (command.equals("log")) {
                 logAndDic.View_log();
